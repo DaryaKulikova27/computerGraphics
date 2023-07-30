341d98752f300cd37794e244f9252848b8e66741
Darya Kulikova <darya.kulikova.02@bk.ru>
2023-07-30 20:13:49 +0300

    Добавила 2 задание 2 лабы

---
+++ lab2/2/MyApp/MainFrm.h
@@ -0,0 +1,69 @@
+#pragma once
+
+using namespace Gdiplus;
+
+class CMainFrame : public CFrameWindowImpl<CMainFrame>, public CUpdateUI<CMainFrame>,
+		public CMessageFilter, public CIdleHandler
+{
+public:
+	CMainFrame();
+	DECLARE_FRAME_WND_CLASS(NULL, IDR_MAIN_MENU)
+
+	virtual BOOL PreTranslateMessage(MSG* pMsg);
+	virtual BOOL OnIdle();
+
+	BEGIN_UPDATE_UI_MAP(CMainFrame)
+	END_UPDATE_UI_MAP()
+
+	BEGIN_MSG_MAP(CMainFrame)
+		MESSAGE_HANDLER(WM_CREATE, OnCreate)
+		MESSAGE_HANDLER(WM_DESTROY, OnDestroy)
+		COMMAND_ID_HANDLER(ID_APP_EXIT, OnFileExit)
+		COMMAND_ID_HANDLER(ID_MENU_OPENFILE, OnFileOpen)
+		COMMAND_ID_HANDLER(ID_MENU_SAVE, OnFileSave)
+		COMMAND_ID_HANDLER(ID_MENU_SAVEAS, OnFileSaveAs)
+		COMMAND_ID_HANDLER(ID_MENU_EXIT, OnFileExit)
+		MESSAGE_HANDLER(WM_SIZE, OnSize)
+		MESSAGE_HANDLER(WM_MOUSEMOVE, OnMouseMove)
+		MESSAGE_HANDLER(WM_LBUTTONDOWN, OnLButtonDown)
+		MESSAGE_HANDLER(WM_LBUTTONUP, OnLButtonUp)
+		MESSAGE_HANDLER(WM_ERASEBKGND, OnEraseBkgnd)
+		MESSAGE_HANDLER(WM_PAINT, OnPaint)
+		CHAIN_MSG_MAP(CUpdateUI<CMainFrame>)
+		CHAIN_MSG_MAP(CFrameWindowImpl<CMainFrame>)
+	END_MSG_MAP()
+
+// Handler prototypes (uncomment arguments if needed):
+//	LRESULT MessageHandler(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+//	LRESULT CommandHandler(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+//	LRESULT NotifyHandler(int /*idCtrl*/, LPNMHDR /*pnmh*/, BOOL& /*bHandled*/)
+
+private:
+	LRESULT OnCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
+	LRESULT OnDestroy(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled);
+	LRESULT OnFileExit(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+	LRESULT OnFileNew(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+	LRESULT OnSize(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
+	LRESULT OnEraseBkgnd(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
+	LRESULT OnPaint(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
+	LRESULT OnFileOpen(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+	LRESULT OnFileSave(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+	LRESULT OnFileSaveAs(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+	LRESULT OnMouseMove(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+	LRESULT OnLButtonDown(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+	LRESULT OnLButtonUp(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+
+	void RedrawBackBuffer(void);
+	CPoint GetRelativePoint(CPoint absoluteCoord);
+
+	std::auto_ptr<Gdiplus::Bitmap> m_pBackBuffer; // то, на чем будет рисоваться
+	std::auto_ptr<Gdiplus::Bitmap> m_pLoadedImage; // картинка
+
+	std::wstring m_fileName;
+	bool m_isDrawing{ false }; // переменная, отвечающая за то, нажата ли левая мышь
+	Gdiplus::Point m_lastPoint{ 0, 0 };
+	Gdiplus::Pen m_pen{ Gdiplus::Color::Black, 10 };
+
+public:
+	void SetDocumentName(std::wstring const& documentName);
+};
